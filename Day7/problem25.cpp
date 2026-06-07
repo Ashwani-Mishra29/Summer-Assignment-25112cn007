fde7305fe074b8341cfc4d929aca7681acc456c6
Ashwani-Mishra29 <25112cn007@glbitm.ac.in>
2026-06-07 23:35:11 +0530

    Add Day 7

---
+++ Day7/problem25.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;  // Base case
+    }
+
+    return n * factorial(n - 1);  // Recursive call
+}
+
+int main() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Factorial of " << n << " = " << factorial(n);
+
+    return 0;
+}
---
+++ Day7/problem28.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int rev = 0;
+
+int reverseNumber(int n) {
+    if (n == 0)
+        return rev;
+
+    rev = rev * 10 + (n % 10);
+    return reverseNumber(n / 10);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Reverse number = " << reverseNumber(n);
+
+    return 0;
+}
