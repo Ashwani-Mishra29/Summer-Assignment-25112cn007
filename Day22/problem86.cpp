412ee7aa98b3844dc7415bc96844ea38616b93c5
Ashwani-Mishra29 <25112cn007@glbitm.ac.in>
2026-06-22 22:03:03 +0530

    Add Day22

---
+++ Day22/problem86.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    int words = 1;
+
+    cout << "Enter a sentence: ";
+    getline(cin, str);
+
+    if (str.length() == 0) {
+        words = 0;
+    } else {
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == ' ') {
+                words++;
+            }
+        }
+    }
+
+    cout << "Number of words = " << words;
+
+    return 0;
+}
